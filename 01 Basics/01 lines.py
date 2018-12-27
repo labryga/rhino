8acaad726479ae032616025354e69695b4478c99
Martin Labryga <martin@labryga.ch>
2018-12-27 21:22:49 +0100

    base lines added

--- 01 Basics/01 lines.py
+++ 01 Basics/01 lines.py
@@ -5,9 +5,6 @@ from random import (randint as ri, uniform as uf)
 from itertools import product, starmap
 
 
-tuple(starmap( lambda k, r, c: rs.ObjectColor(
-    rs.AddCircle(k, r,), c),
-    [ ((0 + i, ri(2,20), 0), ri(2,10), tuple(map(lambda x: ri(0,255), range(3)))) 
-        for i in range(0,200,10) ]))
-
-
+map(lambda x: rs.AddLine(x[0], x[1]), 
+        [ ( (0+i, ri(0,20), 0), (0+i, ri(-20,0), 0) ) 
+            for i in range(0,400,5)])
