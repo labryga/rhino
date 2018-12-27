95cb164473774d0a58cae9dbab40d2d9d019f2c1
Martin Labryga <martin@labryga.ch>
2018-12-27 20:24:07 +0100

    basic circle added

---
+++ 01 Basics/01 circle.py
@@ -0,0 +1,8 @@
+import rhinoscriptsyntax as rs
+import math as mt
+import random
+from random import (randint as ri, uniform as uf)
+from itertools import product, starmap
+
+# x = rs.AddPoint((20, 30, 0))
+rs.AddCircle([20,20,20], 20)
