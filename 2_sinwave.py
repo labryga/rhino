52565368c24468c43cda9257191037060cd49565
Martin Labryga <martin@labryga.ch>
2019-01-08 23:59:21 +0100

    2_sinwave.py altered

--- 2_sinwave.py
+++ 2_sinwave.py
@@ -7,4 +7,7 @@ from math import sin, cos, pi
 #         [(x, 0.05*(x**3)+(x**2)+3, 0) for x in fr(-22,9, 0.5)])
 
 p01 = [(x, 0.05*(x**3)+(x**2)+3, 0) for x in fr(-22,9, 0.5)]
-r.AddInterpCurve(p01)
+r.ObjectColor(r.AddInterpCurve(p01), [255, 255, 0])
+
+p02 = [(x, x**2, 0) for x in fr(-22,9, 0.5)]
+r.ObjectColor(r.AddInterpCurve(p02), [0,255,255])
