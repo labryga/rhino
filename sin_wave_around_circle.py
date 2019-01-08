1e85f24c519c8242132f3dddb19a2c30401281d7
Martin Labryga <martin@labryga.ch>
2019-01-08 23:22:45 +0100

    5test.py alterd

--- sin_wave_around_circle.py
+++ sin_wave_around_circle.py
@@ -2,10 +2,12 @@ import rhinoscriptsyntax as r
 from math import sin, cos
 
 
+p01 = [(sin(x/4), cos(x/4), sin(x)*0.5) for x in r.frange(0.0, 25.12, 0.157)]
 map(lambda x: r.ObjectColor(
-    r.AddPoints(x), [128,255,128] ),
-    [(sin(x/4), cos(x/4), sin(x)*0.5) for x in r.frange(0.0, 25.12, 0.157)] )
+    r.AddPoints(x), [128,255,128] ), p01)
+r.AddCurve(p01)
 
-map(lambda x: r.ObjectColor(
-    r.AddPoints(x), [0,0,255] ),
-    [(sin(x/8), cos(x/8), sin(x)) for x in r.frange(0.0, 50.24, 0.157)] )
+
+# map(lambda x: r.ObjectColor(
+#     r.AddPoints(x), [0,0,255] ),
+#     [(sin(x/8), cos(x/8), sin(x)) for x in r.frange(0.0, 50.24, 0.157)] )
