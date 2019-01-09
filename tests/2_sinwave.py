a5b96394e2fc6f2df0821bc91ed86ee8fea886ff
Martin Labryga <martin@labryga.ch>
2019-01-09 01:26:59 +0100

    rectangle distribution added

--- tests/2_sinwave.py
+++ tests/2_sinwave.py
@@ -1,13 +1,23 @@
 import rhinoscriptsyntax as r
 from rhinoscriptsyntax import frange as fr
 from math import sin, cos, pi
+from random import randint as ri
+from random import uniform as uf
 
 
-# map(r.AddPoints,
-#         [(x, 0.05*(x**3)+(x**2)+3, 0) for x in fr(-22,9, 0.5)])
 
-p01 = [(x, (x**3)+5*(x**2), x**2) for x in fr(-8,4, 0.5)]
-r.ObjectColor(r.AddInterpCurve(p01), [255, 0, 0])
+plane = r.WorldXYPlane()
+kwadrat = r.AddRectangle(plane, 1, 1)
 
-p02 = [(x, 5*(x**2), x**2) for x in fr(-8,4, 0.5)]
-r.ObjectColor(r.AddInterpCurve(p02), [0,0,255])
+p01 = [(x, y, uf(-2, 2)) for x in range(-20,20)
+        for y in range(-20,20)]
+
+p02 = product(*[range(6,10) for i in range(3)])
+
+def ordering(coord):
+    kwadrat_copy = r.CopyObject(kwadrat, coord)
+    zentrum = (coord[0]+0.5, coord[1]+0.5, coord[2]) 
+    r.RotateObject(kwadrat_copy, zentrum, uf(0,360))
+
+
+map(ordering, p01)
